7d9c10f7229d11f28e12968e40e59b60450065eb
ageapps <age.apps.android@gmail.com>
2016-10-11 02:44:31 +0200

    Update Readme

---
+++ Pods/Target Support Files/Pods-CustomUIViewsExamples/Pods-CustomUIViewsExamples-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomUIViewsExamples : NSObject
+@end
+@implementation PodsDummy_Pods_CustomUIViewsExamples
+@end
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CustomUiViewsExamples
+
+Examples of custom *UIViews* using *Swift3*. Begginer practice with Swift language
+
+## Demo
+
+![demo](./art.gif)
